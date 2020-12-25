6816528c5c88f4f09a362ec34d6b41614613e3ce
Adriane <rodrigues.adriane@outlook.com>
2020-12-25 16:42:11 -0300

    Primeiro commit

---
+++ jax-rs-demo/src/main/kotlin/br/com/adriane/demo/jaxrs/JaxRsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.adriane.demo.jaxrs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JaxRsApplication
+
+fun main(args: Array<String>) {
+	runApplication<JaxRsApplication>(*args)
+}
---
+++ jax-rs-demo/src/test/kotlin/br/com/adriane/demo/jaxrs/JaxRsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.adriane.demo.jaxrs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JaxRsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
