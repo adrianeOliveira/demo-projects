2018936167647234ef799feffbd47b6cb087cd6f
Adriane <rodrigues.adriane@outlook.com>
2021-01-19 19:20:19 -0300

    Utilização do avro para disparo de mensagens

--- kotlin-kafka/src/main/kotlin/br/com/adriane/demo/kotlinkafka/KafkaConsumer.kt
+++ kotlin-kafka/src/main/kotlin/br/com/adriane/demo/kotlinkafka/KafkaConsumer.kt
@@ -1,5 +1,6 @@
 package br.com.adriane.demo.kotlinkafka
 
+import br.com.adriane.demo.avro.Contato
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
@@ -12,5 +13,10 @@ class KafkaConsumer {
     @KafkaListener(topics = ["topic-kotlin-app"], groupId = "kotlin-group")
     fun receiveMessage(message : String) {
         log.info("Message received = $message")
+    }
+
+    @KafkaListener(topics = ["topic-kotlin-avro"], groupId = "kotlin-avro-group")
+    fun receiveAvroMessage(contato: Contato) {
+        log.info("Avro message received = $contato")
     }
 }
--- kotlin-kafka/src/main/kotlin/br/com/adriane/demo/kotlinkafka/KafkaController.kt
+++ kotlin-kafka/src/main/kotlin/br/com/adriane/demo/kotlinkafka/KafkaController.kt
@@ -1,5 +1,6 @@
 package br.com.adriane.demo.kotlinkafka
 
+import br.com.adriane.demo.avro.Contato
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,13 +10,27 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RestController
 @RequestMapping("/kafka")
 class KafkaController(
-    val kafkaTemplate: KafkaTemplate<String,String>
+//    val kafkaTemplate: KafkaTemplate<String,String>,
+    val kafkaTemplateContato: KafkaTemplate<String, Contato>
+
 ) {
     private val log = LoggerFactory.getLogger(KafkaController::class.java)
 
-    @GetMapping
-    fun triggerMessageToKaf() {
-        log.info("sending message to kafka")
-        kafkaTemplate.send("topic-kotlin-app", "key", "Hello Kafka from Kotlin")
+//    @GetMapping
+//    fun triggerMessageToKaf() {
+//        log.info("sending message to kafka")
+//        kafkaTemplate.send("topic-kotlin-app", "key", "Hello Kafka from Kotlin")
+//    }
+
+    @GetMapping("/contato")
+    fun triggerAvroMessage() {
+        log.info("Sending message using schema registry with avro")
+
+        val contato = Contato.newBuilder()
+            .setNome("Adriane")
+            .setEmail("email@adriane")
+            .build()
+
+        kafkaTemplateContato.send("topic-kotlin-avro", "avro-key", contato)
     }
 }
