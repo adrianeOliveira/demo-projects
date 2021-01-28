09da9fe9a5dbf742457477bcd0f98755369741af
Adriane <rodrigues.adriane@outlook.com>
2021-01-27 22:15:20 -0300

    setup projeto ddd-demo

---
+++ ddd-demo/src/test/kotlin/br/com/adriane/demo/ddddemo/DddDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.adriane.demo.ddddemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DddDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
