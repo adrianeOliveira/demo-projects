0268c088d200ceae15232dd9259e342b085d7c92
Adriane <rodrigues.adriane@outlook.com>
2022-01-19 20:29:24 -0300

    [reactive-custom-repository] Classe User ajustada para o teste do banco passar na comparação dos objetos

--- reactive-custom-repository/src/main/java/br/com/adriane/demo/reactivecustomrepository/User.java
+++ reactive-custom-repository/src/main/java/br/com/adriane/demo/reactivecustomrepository/User.java
@@ -43,4 +43,23 @@ public class User {
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        return email != null ? email.equals(user.email) : user.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
 }
