3ff88d5d92b3a9eedbb106da538a3dd397fa95f6
Adriane <rodrigues.adriane@outlook.com>
2022-01-19 20:37:36 -0300

    [reactive-custom-repository] Classe UserCustom criada com testes
    Foi criada a classe UserCustom para avaliar o uso de classes de repositorios para retornar dados em classes customizadas a partir da entidade principal

--- reactive-custom-repository/src/main/java/br/com/adriane/demo/reactivecustomrepository/UserRepository.java
+++ reactive-custom-repository/src/main/java/br/com/adriane/demo/reactivecustomrepository/UserRepository.java
@@ -1,8 +1,21 @@
 package br.com.adriane.demo.reactivecustomrepository;
 
+import java.util.List;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface UserRepository  extends ReactiveCrudRepository<User, Integer> {
+
+    @Query("select id, name from users where id = :id")
+    Mono<UserCustom> findUserCustom(@Param("id") Integer id);
+
+    @Query("select id, name from users")
+    Mono<List<UserCustom>> findAllUserCustom();
+
+    @Query("select * from users")
+    Mono<List<User>> findAllUsers();
 }
--- reactive-custom-repository/src/test/java/br/com/adriane/demo/reactivecustomrepository/ReactiveCustomRepositoryApplicationTests.java
+++ reactive-custom-repository/src/test/java/br/com/adriane/demo/reactivecustomrepository/ReactiveCustomRepositoryApplicationTests.java
@@ -1,5 +1,6 @@
 package br.com.adriane.demo.reactivecustomrepository;
 
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
@@ -20,6 +21,50 @@ class ReactiveCustomRepositoryApplicationTests {
 		user.setEmail("rodrigues.adriane@outlook.com");
 		StepVerifier.create(userRepository.findById(1).log())
 				.expectNext(user)
+				.verifyComplete();
+	}
+
+	@Test
+	void findAllUserTest() {
+		User user = new User();
+		user.setId(1);
+		user.setName("Adriane");
+		user.setEmail("rodrigues.adriane@outlook.com");
+		StepVerifier.create(userRepository.findAll().log())
+				.expectNext(user)
+				.verifyComplete();
+	}
+
+	@Test
+	void findUserCustomByIdTest() {
+		UserCustom userCustom = new UserCustom();
+		userCustom.setId(1);
+		userCustom.setName("Adriane");
+
+		StepVerifier.create(userRepository.findUserCustom(1).log())
+				.expectNext(userCustom)
+				.verifyComplete();
+	}
+
+	@Test
+	void findAllUserCustomTest() {
+		UserCustom userCustom = new UserCustom();
+		userCustom.setId(1);
+		userCustom.setName("Adriane");
+
+		StepVerifier.create(userRepository.findAllUserCustom().log())
+				.expectNext(Collections.singletonList(userCustom))
+				.verifyComplete();
+	}
+
+	@Test
+	void findAllUserCustomMonoTest() {
+		UserCustom userCustom = new UserCustom();
+		userCustom.setId(1);
+		userCustom.setName("Adriane");
+
+		StepVerifier.create(userRepository.findAllUserCustom().log())
+				.expectNext(Collections.singletonList(userCustom))
 				.verifyComplete();
 	}
 
