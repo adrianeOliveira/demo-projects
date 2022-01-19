70ec858ab463d50aa3586471f35d71f39218813b
Adriane <rodrigues.adriane@outlook.com>
2022-01-19 19:17:24 -0300

    diretório reactive-custom-repository

---
+++ reactive-custom-repository/src/main/java/br/com/adriane/demo/reactivecustomrepository/ReactiveCustomRepositoryApplication.java
@@ -0,0 +1,13 @@
+package br.com.adriane.demo.reactivecustomrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCustomRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCustomRepositoryApplication.class, args);
+	}
+
+}
---
+++ reactive-custom-repository/src/test/java/br/com/adriane/demo/reactivecustomrepository/ReactiveCustomRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adriane.demo.reactivecustomrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCustomRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
