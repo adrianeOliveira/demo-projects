93cb6ffade021841eb09f1613796b6aa9053c9d8
Adriane <rodrigues.adriane@outlook.com>
2022-03-31 15:29:51 -0300

    gerando relatório populado

--- jasper-report/src/main/java/br/com/adrianerodrigues/demo/JasperReportDemoApplication.java
+++ jasper-report/src/main/java/br/com/adrianerodrigues/demo/JasperReportDemoApplication.java
@@ -1,39 +1,62 @@
 package br.com.adrianerodrigues.demo;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.util.JRSaver;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
+import net.sf.jasperreports.export.SimplePdfReportConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class JasperReportDemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JRException, SQLException {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(JasperReportsConfig.class);
         ctx.refresh();
 
-        SimpleReportFiller simpleReportFiller = ctx.getBean(SimpleReportFiller.class);
-        /*simpleReportFiller.setReportFileName("/resources/employeeEmailReport.jrxml");
-        simpleReportFiller.compileReport();*/
+        InputStream employeeReportStream = JasperReportDemoApplication.class.getResourceAsStream("/employeeReport.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(employeeReportStream);
+        JRSaver.saveObject(jasperReport, "employeeReport.jasper");
+        DataSource dataSource = ctx.getBean(DataSource.class);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, dataSource.getConnection());
 
-        simpleReportFiller.setReportFileName("employeeReport.jrxml");
-        simpleReportFiller.compileReport();
+        exportToPdf(jasperPrint, "employeeReport.pdf", "baeldung");
+    }
 
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("title", "Employee Report Example");
-        parameters.put("minSalary", 15000.0);
-        parameters.put("condition", " LAST_NAME ='Smith' ORDER BY FIRST_NAME");
+    public static void exportToPdf(JasperPrint jasperPrint, String fileName, String author) {
+        JRPdfExporter exporter = new JRPdfExporter();
 
-        simpleReportFiller.setParameters(parameters);
-        simpleReportFiller.fillReport();
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(fileName));
 
-        SimpleReportExporter simpleExporter = ctx.getBean(SimpleReportExporter.class);
-        simpleExporter.setJasperPrint(simpleReportFiller.getJasperPrint());
+        SimplePdfReportConfiguration reportConfig = new SimplePdfReportConfiguration();
+        reportConfig.setSizePageToContent(true);
+        reportConfig.setForceLineBreakPolicy(false);
 
-        simpleExporter.exportToPdf("employeeReport.pdf", "baeldung");
-        /*simpleExporter.exportToXlsx("employeeReport.xlsx", "Employee Data");
-        simpleExporter.exportToCsv("employeeReport.csv");
-        simpleExporter.exportToHtml("employeeReport.html");*/
+        SimplePdfExporterConfiguration exportConfig = new SimplePdfExporterConfiguration();
+        exportConfig.setMetadataAuthor(author);
+        exportConfig.setEncrypted(true);
+        exportConfig.setAllowedPermissionsHint("PRINTING");
+
+        exporter.setConfiguration(reportConfig);
+        exporter.setConfiguration(exportConfig);
+        try {
+            exporter.exportReport();
+        } catch (JRException ex) {
+            Logger.getLogger(SimpleReportFiller.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
