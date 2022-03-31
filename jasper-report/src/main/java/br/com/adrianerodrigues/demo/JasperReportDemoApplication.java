e34dcf9af1d6f63b13f8d0bfed111b252203037b
Adriane <rodrigues.adriane@outlook.com>
2022-03-31 12:22:16 -0300

    feita configuração pra gerar simples relatório em PDF

--- jasper-report/src/main/java/br/com/adrianerodrigues/demo/JasperReportDemoApplication.java
+++ jasper-report/src/main/java/br/com/adrianerodrigues/demo/JasperReportDemoApplication.java
@@ -1,4 +1,39 @@
 package br.com.adrianerodrigues.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
 public class JasperReportDemoApplication {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(JasperReportsConfig.class);
+        ctx.refresh();
+
+        SimpleReportFiller simpleReportFiller = ctx.getBean(SimpleReportFiller.class);
+        /*simpleReportFiller.setReportFileName("/resources/employeeEmailReport.jrxml");
+        simpleReportFiller.compileReport();*/
+
+        simpleReportFiller.setReportFileName("employeeReport.jrxml");
+        simpleReportFiller.compileReport();
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("title", "Employee Report Example");
+        parameters.put("minSalary", 15000.0);
+        parameters.put("condition", " LAST_NAME ='Smith' ORDER BY FIRST_NAME");
+
+        simpleReportFiller.setParameters(parameters);
+        simpleReportFiller.fillReport();
+
+        SimpleReportExporter simpleExporter = ctx.getBean(SimpleReportExporter.class);
+        simpleExporter.setJasperPrint(simpleReportFiller.getJasperPrint());
+
+        simpleExporter.exportToPdf("employeeReport.pdf", "baeldung");
+        /*simpleExporter.exportToXlsx("employeeReport.xlsx", "Employee Data");
+        simpleExporter.exportToCsv("employeeReport.csv");
+        simpleExporter.exportToHtml("employeeReport.html");*/
+    }
 }
